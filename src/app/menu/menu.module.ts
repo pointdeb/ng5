e3362f50cb4a8774a9dfb681eaa75765a1a59c79
Stephano Romain <stephano.romain@gmail.com>
2018-02-18 19:48:33 +0300

    Add footer, Angular Icon

--- src/app/menu/menu.module.ts
+++ src/app/menu/menu.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainMenuComponent } from './main-menu/main-menu.component';
 import { RouterModule } from '@angular/router';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   imports: [
     CommonModule,
     RouterModule
   ],
-  exports: [MainMenuComponent],
-  declarations: [MainMenuComponent]
+  exports: [MainMenuComponent, FooterComponent],
+  declarations: [MainMenuComponent, FooterComponent]
 })
 export class MenuModule { }
